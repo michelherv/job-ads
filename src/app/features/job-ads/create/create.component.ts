c3292d4b15a1e6ea8c3efa2950e71263cd2240db
michelherv <michel-herve.ngounou@simplestis.com>
2023-10-03 23:40:31 +0200

    integrate job ad creation

--- src/app/features/job-ads/create/create.component.ts
+++ src/app/features/job-ads/create/create.component.ts
@@ -1,10 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { JobAd } from '@jobcloud/admin/models/job-ad';
+import { JobAdScannerComponent } from '@jobcloud/libs/job-ad-scanner/job-ad-scanner.component';
+import { Store } from '@ngrx/store';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [JobAdScannerComponent],
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CreateComponent {}
+export default class CreateComponent {
+  constructor(private readonly route: ActivatedRoute, private readonly router: Router, private readonly store: Store) {}
+
+  doSubmit(jobAd: JobAd): void {
+    console.log('creating jobAd', jobAd);
+    // this.store.dispatch(jobAdCreate({ jobAd }));
+  }
+
+  doCancel(): void {
+    this.router.navigate(['..'], { relativeTo: this.route });
+  }
+}
