dc8bbf99d246bde052a7969337613289f6aa1765
michelherv <michel-herve.ngounou@simplestis.com>
2023-10-10 07:33:41 +0200

    backend: improve data sample

--- src/app/fake-backend/job-ad.api.ts
+++ src/app/fake-backend/job-ad.api.ts
@@ -1,12 +1,13 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest, HttpResponse } from '@angular/common/http';
 import { JobAd } from '@jobcloud/admin/models/job-ad';
-import { JobAdStatusEnum } from '@jobcloud/admin/models/job-ad-status.enum';
 import { JobAdDto } from '@jobcloud/admin/models/job-ad.dto';
 import { Observable, of, throwError } from 'rxjs';
+import { generateId, records } from './job-ad.data';
 
 export const JobAdApi = (request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const baseUrl = 'http://jobcloud.test/api';
   const id = request.url.replace(baseUrl, '').match(/\/job-ads\/(?<id>[0-9]+)/)?.groups?.['id'];
+  let items = [...records];
 
   if ('GET' === request.method) {
     if (id) {
@@ -58,136 +59,3 @@ export const JobAdApi = (request: HttpRequest<unknown>, next: HttpHandlerFn): Ob
 
   return throwError(() => new Error('An error occured'));
 };
-
-const generateId = () => Math.ceil(Math.random() * 1000);
-
-let items: JobAdDto[] = [
-  {
-    id: generateId(),
-    title: 'Job Ad 1',
-    description: 'Job Ad 1 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 1,
-        name: 'Company 1',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 2',
-    description: 'Job Ad 2 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 2,
-        name: 'Company 2',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 3',
-    description: 'Job Ad 3 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 3,
-        name: 'Company 3',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 4',
-    description: 'Job Ad 4 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 4,
-        name: 'Company 4',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 5',
-    description: 'Job Ad 5 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 5,
-        name: 'Company 5',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 6',
-    description: 'Job Ad 6 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 6,
-        name: 'Company 6',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 7',
-    description: 'Job Ad 7 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 7,
-        name: 'Company 7',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-  {
-    id: generateId(),
-    title: 'Job Ad 8',
-    description: 'Job Ad 8 Description',
-    skills: ['Skill 1', 'Skill 2'],
-    status: JobAdStatusEnum.Published,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _embedded: {
-      company: {
-        id: 8,
-        name: 'Company 8',
-        logo: 'https://via.placeholder.com/150',
-      },
-    },
-  },
-];
